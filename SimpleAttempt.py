0574a438ee71997c12db8c9d3bfdec2aaf7bfa48
Marielene <annienoncheva@yahoo.com>
2020-03-05 14:09:06 +0000

    updated to categorical LSTM NN

--- SimpleAttempt.py
+++ SimpleAttempt.py
@@ -23,7 +23,7 @@ def load_doc(filename):
 	# close the file
 	file.close()
 	return text
-#find biggest list entry in a list.
+# find biggest list entry in a list of lists, since len only gives you the largest single element.
 def count_max(count_list):
 	maxboi=0
 	for item in count_list:
@@ -52,19 +52,26 @@ sequences = tokenizer.texts_to_sequences(tweets)
 standard=count_max(sequences)
 sequences_final=sq.pad_sequences(sequences, padding='post', maxlen=standard)
 # organize data.
-X_train = sequences_final
-tempp=prep_data_int(emoji)
-y_train= to_categorical(tempp, num_classes=None)
-# define Model
+delimeter_train=32000000
+X_train = sequences_final[:delimeter_train, :]
+X_test=sequences_final[delimeter_train:, :]
+emoji_prep=prep_data_int(emoji)
+y= to_categorical(emoji_prep, num_classes=None)
+y_train, y_test = y[:delimeter_train], y[delimeter_train:]
+# define model
 model = Sequential()
+# embed list of words
 model.add(Embedding(vocab_size, 100, input_length=standard))
 model.add(LSTM(100, return_sequences=True))
 model.add(LSTM(100, return_sequences=True))
 model.add(LSTM(100))
 model.add(Dense(20, activation='softmax'))
+# compile and print summary of model
 model.compile(optimizer='ADAM', loss='categorical_crossentropy', metrics=['accuracy'])
 print(model.summary())
+# tuning learn rate if it stagnates through the epochs.
 reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=3, min_lr=0.001)
+# fit model and train
 model.fit(X_train, y_train, epochs=32, batch_size=1024, callbacks=[reduce_lr], verbose=1)
 # save the model to file
 model.save('model.h5')
@@ -72,15 +79,6 @@ model.save('model.h5')
 dump(tokenizer, open('tokenizer.pkl', 'wb'))
 # Final evaluation of the model
 print("Evaluation section, please stand by.")
-in_filenameX = 'processedSmallBatch.txt'
-in_filenameY = 'processedEmojiSmallBatch.txt'
-docx_train = load_doc(in_filenameX)
-docy_train = load_doc(in_filenameY)
-tweets= docx_train.split('\n')
-emoji= docy_train.split('\n')
-X_test=prep_data_txt(tweets, tokenizer)
-tempr=prep_data_int(emoji)
-y_test= to_categorical(tempr, num_classes=None)
 kfold = KFold(n_splits=10, shuffle=True)
 results = cross_val_score(model, X_test, y_test, cv=kfold)
 print("Baseline:" + str(results.mean()*100) +" ("+ str(results.std()*100) +")")

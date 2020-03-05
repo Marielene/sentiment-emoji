e66ed029726773391f75b22e2cdf23e9521bd90f
Marielene <annienoncheva@yahoo.com>
2020-03-05 13:57:11 +0000

    updated to categorical LSTM NN

--- SimpleAttempt.py
+++ SimpleAttempt.py
@@ -8,7 +8,8 @@ from keras.preprocessing.text import Tokenizer
 from keras.layers.core import Activation, Dropout, Dense
 from keras.layers import Flatten
 from keras.layers import MaxPooling1D, Conv1D
-#from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical
 from keras.callbacks import ReduceLROnPlateau
@@ -22,20 +23,14 @@ def load_doc(filename):
 	# close the file
 	file.close()
 	return text
-#find biggest LIST entry
+#find biggest list entry in a list.
 def count_max(count_list):
 	maxboi=0
 	for item in count_list:
 		if(maxboi<len(item)):
 			maxboi=len(item)
 	return maxboi
-def prep_data_txt(raw_data, tokenizer):
-	tokenizer.fit_on_texts(raw_data)
-	sequences = tokenizer.texts_to_sequences(raw_data)
-	unique_words=len(np.unique(sequences))
-	standard=count_max(sequences)
-	sequences=sq.pad_sequences(sequences, padding='post', maxlen=standard)
-	return sequences
+# map integer array
 def prep_data_int(raw_int):
 	temp=list(map(int, raw_int))
 	raw_int=np.array(temp)
@@ -47,19 +42,22 @@ docx_train = load_doc(in_filenameX)
 docy_train = load_doc(in_filenameY)
 tweets= docx_train.split('\n')
 emoji= docy_train.split('\n')
-# integer encode sequences of words
+# declare tokenizer
 tokenizer = Tokenizer()
+tokenizer.fit_on_texts(tweets)
 # vocabulary size
 vocab_size = len(tokenizer.word_index) + 1
-# standardize input
-X_train = prep_data_txt(tweets, tokenizer)
-size_input=len(X_train[1])
+sequences = tokenizer.texts_to_sequences(tweets)
+#unique_words=len(np.unique(sequences))
+standard=count_max(sequences)
+sequences_final=sq.pad_sequences(sequences, padding='post', maxlen=standard)
+# organize data.
+X_train = sequences_final
 tempp=prep_data_int(emoji)
 y_train= to_categorical(tempp, num_classes=None)
+# define Model
 model = Sequential()
-model.add(Embedding(vocab_size, 100, input_length=size_input))
-#model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-#model.add(MaxPooling1D(pool_size=2))
+model.add(Embedding(vocab_size, 100, input_length=standard))
 model.add(LSTM(100, return_sequences=True))
 model.add(LSTM(100, return_sequences=True))
 model.add(LSTM(100))
@@ -68,10 +66,21 @@ model.compile(optimizer='ADAM', loss='categorical_crossentropy', metrics=['accur
 print(model.summary())
 reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=3, min_lr=0.001)
 model.fit(X_train, y_train, epochs=32, batch_size=1024, callbacks=[reduce_lr], verbose=1)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: " + str(scores[1]*100) + "%")
 # save the model to file
 model.save('model.h5')
 # save the tokenizer
 dump(tokenizer, open('tokenizer.pkl', 'wb'))
+# Final evaluation of the model
+print("Evaluation section, please stand by.")
+in_filenameX = 'processedSmallBatch.txt'
+in_filenameY = 'processedEmojiSmallBatch.txt'
+docx_train = load_doc(in_filenameX)
+docy_train = load_doc(in_filenameY)
+tweets= docx_train.split('\n')
+emoji= docy_train.split('\n')
+X_test=prep_data_txt(tweets, tokenizer)
+tempr=prep_data_int(emoji)
+y_test= to_categorical(tempr, num_classes=None)
+kfold = KFold(n_splits=10, shuffle=True)
+results = cross_val_score(model, X_test, y_test, cv=kfold)
+print("Baseline:" + str(results.mean()*100) +" ("+ str(results.std()*100) +")")

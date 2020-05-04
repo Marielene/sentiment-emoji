2ceb5933b53dd1f79c48073ac0bc830f46fa731f
Freya Noncheva <annienoncheva@yahoo.com>
2020-05-04 11:49:44 +0100

    Updated code

--- SentimentAttempt.py
+++ SentimentAttempt.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from keras.preprocessing.text import Tokenizer
@@ -6,8 +5,9 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, SpatialDropout1D, GRU, Embedding
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
 import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from keras.models import model_from_json
 from pickle import dump
 #Beauty, grace
@@ -17,30 +17,34 @@ def count_max(count_list):
 		if(maxboi<len(item)):
 			maxboi=len(item)
 	return maxboi
-
+  # Vars to clean data
+lemmatizer = WordNetLemmatizer();
+StopW=list(stopwords.words('english'))
+StopW.append('ive')
+StopW.append('youve')
+StopW.append('im')
+StopW.append('user')
  # Load data
 cols = ['sentiment','id','date','query_string','user','text']
 data = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding='latin-1', names=cols)
 data = data[['text','sentiment']]
 data['text'] = data['text'].apply((lambda x: re.sub('[^a-zA-z\s]','',x)))
 data['text'] = data['text'].apply(lambda x: x.lower())
+data['text']= data['text'].apply((lambda x: lemmatizer.lemmatize(x)))
+#for word in StopW:
+#  data['text']= data['text'].apply((lambda x: re.sub(r"\b" + word + r"\b", "",x)))
 for idx,row in data.iterrows():
     row[0] = row[0].replace('rt',' ')
 #Prepare data
-max_features = 10000
-tokenizer = Tokenizer(num_words=max_features)
+#max_features = 10000
+tokenizer = Tokenizer()
 tokenizer.fit_on_texts(data['text'].values)
 X = tokenizer.texts_to_sequences(data['text'].values)
 standard=count_max(X)
 vocab_size = len(tokenizer.word_index) + 1
 X = pad_sequences(X, maxlen=standard)
 
-
 Y = pd.get_dummies(data['sentiment'].values)
-#vectorizer = CountVectorizer()
-#vectorizer.fit(data['text'].values)
-#temp=vectorizer.stop_words('english', data['text'].values)
-#X = vectorizer.transform(data['text'].values)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33, random_state = 42)
 print(X_train.shape,Y_train.shape)
@@ -58,16 +62,14 @@ Y_test = Y_test[:-validation_size]
 input_dim = X_train.shape[0]  # Number of features
 
 model = Sequential()
-model.add(Embedding(10000, 30, input_length=standard))
+model.add(Embedding(vocab_size, 32, input_length=standard))
 model.add(SpatialDropout1D(0.2))
-model.add(LSTM(100, return_sequences=True))
-model.add(LSTM(100, return_sequences=True))
 model.add(LSTM(100))
 model.add(Dense(2, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='ADAM', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 print(model.summary())
  # fit model
-model.fit(X_train, Y_train, epochs=10, verbose=1, validation_split=0.0001, batch_size=128)
+model.fit(X_train, Y_train, epochs=10, shuffle=True, verbose=1, validation_split=0.001, batch_size=128)
 #print(model.summary())
 #Evaluate Model
 score = model.evaluate(X_test,Y_test)
@@ -82,74 +84,3 @@ print("Saved model to disk")
 
 # save the tokenizer
 dump(tokenizer, open('tokenizerSent.pkl', 'wb'))
-=======
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D, GRU
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer
-from keras.utils.np_utils import to_categorical
-import re
-from keras.models import model_from_json
-from pickle import dump
- # Load data
-cols = ['sentiment','id','date','query_string','user','text']
-data = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding='latin-1', names=cols)
-data = data[['text','sentiment']]
-data['text'] = data['text'].apply((lambda x: re.sub('[^a-zA-z\s]','',x)))
-data['text'] = data['text'].apply(lambda x: x.lower())
-for idx,row in data.iterrows():
-    row[0] = row[0].replace('rt',' ')
-#Prepare data
-#max_fatures = 2000
-#tokenizer = Tokenizer(num_words=max_fatures, split=' ')
-#tokenizer.fit_on_texts(data['text'].values)
-#X = tokenizer.texts_to_sequences(data['text'].values)
-#X = pad_sequences(X)
-
-Y = pd.get_dummies(data['sentiment'].values)
-vectorizer = CountVectorizer()
-vectorizer.fit(data['text'].values)
-X = vectorizer.transform(data['text'].values)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33, random_state = 42)
-print(X_train.shape,Y_train.shape)
-print(X_test.shape,Y_test.shape)
- # fit model
-batch_size = 32
-
-# Validate model
-validation_size = 1500
-X_validate = X_test[-validation_size:]
-Y_validate = Y_test[-validation_size:]
-X_test = X_test[:-validation_size]
-Y_test = Y_test[:-validation_size]
-
-input_dim = X_train.shape[1]  # Number of features
-
-model = Sequential()
-model.add(Dense(100, input_dim=input_dim, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(2, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='ADAM', metrics=['accuracy'])
-print(model.summary())
-
-model.fit(X_train, Y_train, epochs=10, verbose=1, validation_data=(X_validate, Y_validate), batch_size=32)
-score = model.evaluate(X_test,Y_test)
-print("Accuracy:", score)
-# save the model to file
-model_json = model.to_json()
-with open("modelSent.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("modelSent.h5")
-print("Saved model to disk")
-
-# save the tokenizer
-#dump(tokenizer, open('tokenizerSent.pkl', 'wb'))
->>>>>>> 6e5f14238ff2b064d58bfb5962ee365eca3822f2
--- SimpleAttempt.py
+++ SimpleAttempt.py
@@ -1,14 +1,14 @@
 import numpy as np
 import os
+from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import LSTM, GRU, SimpleRNN
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence as sq
 from keras.preprocessing.text import Tokenizer
-from keras.layers.core import Activation, Dropout, Dense
-from keras.layers import Flatten
-from keras.layers import MaxPooling1D, SpatialDropout1D
+from keras.layers.core import Activation, Dropout
+from keras.layers import GlobalMaxPool1D, SpatialDropout1D
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical
@@ -40,6 +40,7 @@ def prep_data_int(raw_int):
 	raw_int=np.array(temp)
 	return raw_int
 
+
 # load prepped data
 in_filenameX = 'processed.txt'
 in_filenameY = 'processedEmoji.txt'
@@ -49,7 +50,7 @@ tweets= docx_train.split('\n')
 emoji= docy_train.split('\n')
 
 # declare tokenizer
-tokenizer = Tokenizer(num_words=100000)
+tokenizer = Tokenizer()
 tokenizer.fit_on_texts(tweets)
 # vocabulary size
 vocab_size = len(tokenizer.word_index) + 1
@@ -59,29 +60,37 @@ sequences_final=sq.pad_sequences(sequences, padding='post', maxlen=standard)
 unique_words=len(np.unique(sequences_final))
 # organize data.
 emoji_prep=prep_data_int(emoji)
-X_train=sequences_final
-y_train= to_categorical(emoji_prep, num_classes=None)
-# Split the data
-
+X_test=sequences_final[-1000:]
+X_train=sequences_final[:-1000]
+Y_test=emoji_prep[-1000:]
+y_train=emoji_prep[:-1000]
+y_train= to_categorical(y_train, num_classes=None)
+Y_test= to_categorical(Y_test, num_classes=None)
+size_batch=24
 # define model
+#print(X_train.shape[1])
 model = Sequential()
-model.add(Embedding(100000, 20, input_length=standard))
-model.add(LSTM(100, return_sequences=True))
-model.add(LSTM(100, return_sequences=True))
-model.add(LSTM(100))
+model.add(Embedding(unique_words, 32, trainable=True))
+model.add(LSTM(128, recurrent_dropout=0.2, return_sequences=True))
+model.add(LSTM(64, recurrent_dropout=0.2, return_sequences=True))
+model.add(LSTM(32))
 model.add(Dense(20, activation='softmax'))
 
 # compile and print summary of model
-model.compile(optimizer='ADAM', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 print(model.summary())
 
 # tuning learn rate if it stagnates through the epochs.
-reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=3, min_lr=0.001)
+#reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=3, min_lr=0.001)
 
 # fit model and train
-model.fit(X_train, y_train, epochs=20, batch_size=32, callbacks=[reduce_lr], verbose=1, validation_split=0.25)
-
-results=model.evaluate()
+for i in range(10):
+	model.fit(X_train, y_train, epochs=1, batch_size=size_batch, verbose=1, shuffle=False)
+	model.reset_states()
+#model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=1, validation_split=0.01, shuffle=True)
+# evaluate trained model
+#score = model.evaluate(X_test,Y_test)
+#print("Accuracy:", score[1], "Loss:", score[0])
 
 # save the model to file
 model_json = model.to_json()
@@ -96,15 +105,15 @@ dump(tokenizer, open('tokenizer.pkl', 'wb'))
 
 #eval hopefully
 #load json and create model
-#json_file = open('model.json', 'r')
-#loaded_model_json = json_file.read()
-#json_file.close()
-#loaded_model = model_from_json(loaded_model_json)
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-#loaded_model.load_weights("model.h5")
-#print("Loaded model from disk")
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
  
 # evaluate loaded model on test data
-#loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-#score = model.evaluate(X_test, y_test, verbose=1)
-#print("Percentage:" + ( np.mean(score)*100) + "%")
+loaded_model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+score = model.evaluate(X_test, Y_test, verbose=1)
+print("Percentage:" + score*100 + "%")
